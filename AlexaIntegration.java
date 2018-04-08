83f48a7baf4cb6d3aaa6ca9def909a56158b6d6e
nsalomonb <nsalomon.b@gmail.com>
2018-04-07 21:10:01 -0400

    Update 1

--- AlexaIntegration.java
+++ AlexaIntegration.java
@@ -8,56 +8,43 @@ public class AlexaIntegration{
    }
    
    public AlexaIntegration(){
-      // The name of the file to open.
-        String fileName = "AlexaControl.txt";
-
-        // This will reference one line at a time
-        String line = null;
-
-        try {
-            // FileReader reads text files in the default encoding.
-            FileReader fileReader = 
-                new FileReader(fileName);
-
-            // Always wrap FileReader in BufferedReader.
-            BufferedReader bufferedReader = 
-                new BufferedReader(fileReader);
-
-            line = bufferedReader.readLine(); 
-
-            // Always close files.
-            bufferedReader.close();         
-        }
-        catch(FileNotFoundException ex) {
-            System.out.println(
-                "Unable to open file '" + 
-                fileName + "'");                
-        }
-        catch(IOException ex) {
-            System.out.println(
-                "Error reading file '" 
-                + fileName + "'");                  
-            // Or we could just do this: 
-            // ex.printStackTrace();
-        }
-  
+      String fileName = "AlexaControl.txt";
+      String line = null;
+      File file = new File(fileName);
+      
+      try{         
+         Scanner sc = new Scanner(file);         
+         line = sc.nextLine();
+         if(line.equals("")){
+            line = sc.nextLine();
+         }
+         System.out.println(line);
+         sc.close();
+      }catch(FileNotFoundException e){
+         e.printStackTrace();
+      }
+        
       try {
-         // Execute command
          String command = "cmd /c start " + line;
          Process child = Runtime.getRuntime().exec(command);
-
-         // Get output stream to write from it
-         OutputStream out = child.getOutputStream();
+         OutputStream out = child.getOutputStream();      
+            out.write("cd C:/ /r/n".getBytes());
+            out.flush();
+            out.write("dir /r/n".getBytes());
+            out.close();
+      }catch(IOException e){}
       
-         out.write("cd C:/ /r/n".getBytes());
-         out.flush();
-         out.write("dir /r/n".getBytes());
-         out.close();
-      } catch (IOException e) {
+      try{
+         Runtime.getRuntime().exec("taskkill /f /im cmd.exe");  
+         if(file.delete()){
+            System.out.println("File deleted successfully");
+         }
+         else{
+            System.out.println("Failed to delete the file");
+         }
+            
+      }catch(Exception e){
+         e.printStackTrace();  
       }
-   
    }
-
-
-
 }
